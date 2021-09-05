6c947aa1a63ed32a4371f364e7d3d903d2d06fa8
AbdelrhmanEzzat <64223277+AbdelrhmanEzzat@users.noreply.github.com>
2021-09-06 00:11:44 +0200

    Update TASK-1.php

--- TASK-1.php
+++ TASK-1.php
@@ -3,20 +3,20 @@
 $First_cost=3.50;
 $second_cost=4.00;
 $third_cost=6.50;
-$units= 190;
+$units= 190; // assume units 
 
 if ($units <= 50){
 
-    echo $bill = $units * 3.50; 
+    echo " the bill = ". $bill = $units * 3.50; 
 }
 elseif($units > 50 && $units >= 100){
 
-    echo $bill = $units * 4.00;
+    echo " the bill = ". $bill = $units * 4.00;
 }
 
 else{
-    echo $bill = $units * 6.50;
+    echo  "the bill = ". $bill = $units * 6.50;
 }
-
+// output will be  "the bill = 760" in third COST
 
 ?>
