de8a96d662d9dd5e7af8be6366ca079fd703255b
AbdelrhmanEzzat <64223277+AbdelrhmanEzzat@users.noreply.github.com>
2021-09-06 09:24:30 +0200

    Update TASK-1.php

--- TASK-1.php
+++ TASK-1.php
@@ -20,3 +20,30 @@ else{
 // output will be  "the bill = 760" in third COST
 
 ?>
+
+
+// new Updating
+/*
+  $unit_cost_first = 3.50;
+    $unit_cost_second = 4.00;
+    $unit_cost_third = 6.50;
+
+    if($units <= 50) {
+        $bill = $units * $unit_cost_first;
+    }
+    else if($units > 50 && $units <= 100) {
+        $temp = 50 * $unit_cost_first;
+        $remaining_units = $units - 50;
+        echo $bill = $temp + ($remaining_units * $unit_cost_second);
+    }
+    else if($units > 100 && $units <= 200) {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second);
+        $remaining_units = $units - 150;
+        echo $bill = $temp + ($remaining_units * $unit_cost_third);
+    }
+    else {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second) + (100 * $unit_cost_third);
+        $remaining_units = $units - 250;
+        echo $bill = $temp + ($remaining_units * $unit_cost_fourth);
+    }
+*/
